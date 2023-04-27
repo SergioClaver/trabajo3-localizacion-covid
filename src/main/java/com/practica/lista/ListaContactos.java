c49b4e3d0342a99f0231fc342f39be9b57fee330
SergioClaver <124038044+SergioClaver@users.noreply.github.com>
2023-04-27 10:34:43 +0200

    Update ListaContactos.java

--- src/main/java/com/practica/lista/ListaContactos.java
+++ src/main/java/com/practica/lista/ListaContactos.java
@@ -34,39 +34,7 @@ public class ListaContactos {
 	}
 }
 private void noHaceCosas2(PosicionPersona p, NodoTemporal aux, NodoTemporal ant){
-    NodoTemporal nuevo = new NodoTemporal();
-    nuevo.setFecha(p.getFechaPosicion());
 
-    
-    NodoPosicion npActual = nuevo.getListaCoordenadas();
-    NodoPosicion npAnt=null;	
-    boolean npEncontrado = false;
-    while (npActual!=null && !npEncontrado) {
-        if(npActual.getCoordenada().equals(p.getCoordenada())) {
-            npEncontrado=true;
-            npActual.setNumPersonas(npActual.getNumPersonas()+1);
-        }else {
-            npAnt = npActual;
-            npActual = npActual.getSiguiente();
-        }
-    }
-    if(!npEncontrado) {
-        NodoPosicion npNuevo = new NodoPosicion(p.getCoordenada(),  1, null);				
-        if(nuevo.getListaCoordenadas()==null)
-            nuevo.setListaCoordenadas(npNuevo);
-        else
-            npAnt.setSiguiente(npNuevo);			
-    }
-    
-    if(ant!=null) {
-        nuevo.setSiguiente(aux);
-        ant.setSiguiente(nuevo);
-    }else {
-        nuevo.setSiguiente(lista);
-        lista = nuevo;
-    }
-    this.size++;
-    
 }
 public void insertarNodoTemporal (PosicionPersona p) {
 		NodoTemporal aux = lista, ant=null;
@@ -95,7 +63,39 @@ public void insertarNodoTemporal (PosicionPersona p) {
 		 * metemos un nodo nuevo en la lista
 		 */
 		if(!encontrado) {
-			noHaceCosas2(p, aux, ant);
+			NodoTemporal nuevo = new NodoTemporal();
+			nuevo.setFecha(p.getFechaPosicion());
+
+			
+			NodoPosicion npActual = nuevo.getListaCoordenadas();
+			NodoPosicion npAnt=null;	
+			boolean npEncontrado = false;
+			while (npActual!=null && !npEncontrado) {
+				if(npActual.getCoordenada().equals(p.getCoordenada())) {
+					npEncontrado=true;
+					npActual.setNumPersonas(npActual.getNumPersonas()+1);
+				}else {
+					npAnt = npActual;
+					npActual = npActual.getSiguiente();
+				}
+			}
+			if(!npEncontrado) {
+				NodoPosicion npNuevo = new NodoPosicion(p.getCoordenada(),  1, null);				
+				if(nuevo.getListaCoordenadas()==null)
+					nuevo.setListaCoordenadas(npNuevo);
+				else
+					npAnt.setSiguiente(npNuevo);			
+			}
+			
+			if(ant!=null) {
+				nuevo.setSiguiente(aux);
+				ant.setSiguiente(nuevo);
+			}else {
+				nuevo.setSiguiente(lista);
+				lista = nuevo;
+			}
+			this.size++;
+			
 		}
 	}
 	
